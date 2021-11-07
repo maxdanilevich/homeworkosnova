7bb740ed9616f159c858737f7f1d14ebf3d51979
maxdanilevich <finskiiiiy2107@gmail.com>
2021-11-07 14:57:10 +0300

    update

--- main.py
+++ main.py
@@ -1,48 +1,9 @@
-print("Задача 72")
-coun = ['Математика', 'Русский', 'Белорусский', 'Информатика', 'Биология', 'Английский']
-for country in coun:
-    print(country)
-iyuy=input("Введите какие предметы вам не нравятся: ")
-if iyuy=='Математика':
-    coun.pop(0)
-if iyuy=='Русский':
-    coun.pop(1)
-if iyuy=='Белорусский':
-    coun.pop(2)
-if iyuy=='Информатика':
-    coun.pop(3)
-if iyuy=='Биология':
-    coun.pop(4)
-if iyuy=='Английский':
-    coun.pop(5)
-for country in coun:
-    print(country)
-print("Задача 74")
-color=['Фиолетовый','Красный','Желтый','Синий','Серый','Черный','Белый','Пурпурный','Голубой','Зеленый']
-x=int(input("Выберите число от 0 до 4"))
-y=int(input("Выберите число от 5 до 9"))
-del color[0:x]
-del color[y-1:9]
-print(color)
-print("Задача 75")
-chisla=[123,456,789,458]
-print(chisla)
-a=int(input("Введите трехзначное число"))
-if a==chisla[0]:
-    print(1)
-elif a==chisla[1]:
-    print(2)
-elif a==chisla[2]:
-    print(3)
-elif a==chisla[3]:
-    print(4)
-else:
-    print("That is not in the list")
-print("Задача 76")
+print("Задача 77")
 w=input("введите имя первого человека: ")
 y=input("введите имя второго человека: ")
 t=input("введите имя третьего человека: ")
 rty=[w,y,t]
+print(rty)
 while True:
     word=input("Хотите позвать еще одного человека?")
     if word=="yes":
@@ -50,4 +11,13 @@ while True:
         rty.append(o)
     if word=="no":
         break;
-print(len(rty))
+print(rty)
+while True:
+    word1=input("Введите имя человека из списка")
+    k=(rty.index(word1))
+    print(k+1)
+    message=input("Хотите ли вы, чтобы этот человек присутствовал на вечеринке")
+    if message=="no":
+        rty.pop(k)
+        break;
+print(rty)
