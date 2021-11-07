ecaec685d6406e4e49d4b6fa3c66d51edc1eac6a
maxdanilevich <finskiiiiy2107@gmail.com>
2021-11-07 15:26:14 +0300

    update

--- main.py
+++ main.py
@@ -21,3 +21,27 @@ while True:
         rty.pop(k)
         break;
 print(rty)
+
+print("Задача 78")
+tv=["Major","Gdechtokogda","Pogoda","News"]
+for i in tv:
+    print(i)
+fy=input("Введите название еще одной программы")
+gy=int(input("введите позицию этой программы в списке"))
+tv.insert(gy-1,fy)
+for i in tv:
+    print(i)
+
+print("Задача 79")
+nums=[]
+o=0
+while True:
+    o+=1
+    u=int(input("Введите число"))
+    nums.append(u)
+    if o==3:
+        question=input("Хотите ли вы оставить последнее введенное число в списке?")
+        if question=="no":
+            nums.pop(2)
+            break;
+print(nums)
